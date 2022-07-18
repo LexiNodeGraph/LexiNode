5f3e361ff2ddd15a9f0ea7718bc4e277b54c584f
ogabrielpereira <ogabrielpereira@pm.me>
2022-07-18 12:45:52 -0300

    graph size

--- front/src/views/Grafico.vue
+++ front/src/views/Grafico.vue
@@ -1,6 +1,5 @@
 <template>
-  <iframe width="100%" height="1000px" allowfullscreen="allowfullscreen" src="http://localhost:3000/demo" title="Sigma.js demo"></iframe>
-  <div class="mt-40"></div>
+  <iframe width="100%" height="850px" allowfullscreen="allowfullscreen" src="http://localhost:3000/demo" title="Sigma.js demo"></iframe>
 </template>
 
 <script>
@@ -9,6 +8,3 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
