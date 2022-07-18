ab5efc9cf785c398a8078243c0b84894432fd36f
ogabrielpereira <ogabrielpereira@pm.me>
2022-07-18 17:31:45 -0300

    user picture added to navbar

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -3,9 +3,10 @@ import {useAuth0} from "@auth0/auth0-vue";
                             
 export default {
     setup() {
-        const {isAuthenticated} = useAuth0();
+        const {isAuthenticated, user } = useAuth0();
         return {
             isAuthenticated,
+            user
         };
     },
 }
@@ -27,23 +28,37 @@ export default {
                     <!-- FIM LOGO -->
 
                     <!-- MENU -->
-                    <div class="flex items-center py-4 px-2" v-if="!showMenu">
+                    <div class="flex items-center py-2 px-2">
                         
                         <router-link class="py-2 px-6 font-semibold text-white rounded hover:bg-[#ffffff11] transition duration-300 mx-1" to="/artigos">
                             <span>Artigos</span>
                         </router-link>
 
-                        <!-- <router-link class="py-2 px-6 font-semibold text-white rounded hover:bg-[#ffffff11] transition duration-300 mx-1" to="/">
-                            <span>Cadastrar Artigo</span>
-                        </router-link> -->
-
                         <router-link class="py-2 px-6 font-semibold text-white rounded hover:bg-[#ffffff11] transition duration-300 mx-1" to="/autores">
                             <span>Autores</span>
                         </router-link>
 
-                        <router-link class="py-2 px-6 font-semibold text-white rounded hover:bg-[#ffffff11] transition duration-300 mx-1" to="/login">
+                        <router-link 
+                        class="py-2 px-6 font-semibold text-white rounded hover:bg-[#ffffff11] transition duration-300 mx-1" 
+                        to="/login"
+                        v-if="!isAuthenticated"
+                        >
                             <span>Entrar</span>
                         </router-link>
+
+                        <router-link 
+                        class="py-2 px-6 rounded-2xl bg-black hover:bg-[#ffffff11] transition duration-300" 
+                        to="/login"
+                        v-if="isAuthenticated"
+                        >
+                            <img 
+                            class="rounded-2xl w-12 h-12" 
+                            :src="user.picture"
+                            referrerpolicy="no-referrer"
+                            >
+
+                        </router-link>
+
                     </div>
                     <!-- FIM MENU -->
                 </div>
