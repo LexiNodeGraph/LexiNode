084f80f04704e0c0fa41258697c7a007a05e8381
Gabriel L. Pereira <ogabrielpereira@pm.me>
2022-07-20 10:56:26 -0300

    Update [...auth0].js

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,9 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+    export default handleAuth({
+      async login(request, response) {
+        await handleLogin(request, response, {
+          returnTo: '/',
+        });
+      },
+    });
