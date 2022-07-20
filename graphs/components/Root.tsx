0b01068cb30bd4d216ee6e246908619f4e60d6e5
Gabriel L. Pereira <ogabrielpereira@pm.me>
2022-07-20 10:29:26 -0300

    Update Root.tsx

--- graphs/components/Root.tsx
+++ graphs/components/Root.tsx
@@ -40,7 +40,7 @@ const Root: FC = () => {
     // if(process.env.prod == "True"){
 
     // }
-    fetch(`https://lexinode-lexiteam.vercel.app/api/dataset`)
+    fetch(`https://lexinode.vercel.app//api/dataset`)
       .then((res) => res.json())
       .then((dataset: Dataset) => {
         setDataset(dataset);
