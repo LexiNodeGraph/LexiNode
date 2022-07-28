86b0e105d410b2a8fdbbc748ca921ff813734e1c
OGabrielPereira <ogabrielpereira@pm.me>
2022-07-27 23:33:56 -0300

    profile image

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
 module.exports = {
   nextConfig,
   images: { 
-    domains: ['lh3.googleusercontent.com', 'i.imgur.com'],
+    domains: ['lh3.googleusercontent.com', 'i.imgur.com', 'imgs.search.brave.com'],
   }
 
 }
--- pages/perfil/index.tsx
+++ pages/perfil/index.tsx
@@ -13,12 +13,11 @@ const Perfil = () => {
             <NavBar />
 
             
-
             <Link href="/perfil">
                 <a 
                 className="flex justify-center mt-10"
                 >
-                    <Image className='rounded-2xl' src={user?.picture || "https://i.imgur.com/eRWRaqG.png"} alt={user?.name || "Não encontrado"} width='100' height='100'  />
+                    <Image className='rounded-2xl' src={user?.picture || "https://imgs.search.brave.com/95uqrX0sPNStAH-hNb9IC1Dq-eZQGRKKEUFV6PMDY_U/rs:fit:1200:1200:1/g:ce/aHR0cDovL3d3dy5k/cm9kZC5jb20vaW1h/Z2VzMTQvd2hpdGUx/LmpwZw"} alt={user?.name || "Não encontrado"} width='100' height='100'  />
                 </a>
 
             </Link>
