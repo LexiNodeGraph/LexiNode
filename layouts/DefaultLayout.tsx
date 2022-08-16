22a8e6e1f95f231542fb755eeb3ed5f85678cfa3
OGabrielPereira <ogabrielpereira@pm.me>
2022-08-16 11:49:33 -0400

    Title && favicon added

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Footer from "../components/Footer";
 import NavBar from "../components/Navbar";
+import Head from "next/head";
 
 function DefaultLayout({children}: any) {
     return (
         <>
+            <Head>
+                <title>LEXINODE</title>
+                <link rel="icon" href="/favicon.png" />
+            </Head>
             <NavBar />
             <div>{children}</div>
             <Footer />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import Root from "../graphs/components/Root";
-
 import React from "react";
 
 const Home = () => {
     return (
-        <main className="fixed h-screen w-full inset-x-0 top-20 z-0">
-            <Root />
-        </main>
+        <>
+            <main className="fixed h-screen w-full inset-x-0 top-20 z-0">
+                <Root />
+            </main>
+        </>
     );
 };
 
