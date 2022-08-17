ceaf23ce2f547c8da57e806663b2c30584a00156
OGabrielPereira <ogabrielpereira@pm.me>
2022-08-17 08:44:42 -0400

    Author page updated

--- pages/artigos/index.tsx
+++ pages/artigos/index.tsx
@@ -36,10 +36,10 @@ const Artigos = () => {
                     type="text"
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
-                    className="block p-4 mx-2 pl-10 w-4/5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="block p-4 mx-2 pl-10 w-4/5 text-sm text-white bg-black rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                     placeholder="Pesquiar artigos"
                 />
-                <select className="bg-gray-50 border w-1/5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select className="bg-black border w-1/5 border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5">
                     <option selected>Filtar por</option>
                 </select>
             </div>
--- pages/perfil/index.tsx
+++ pages/perfil/index.tsx
@@ -8,22 +8,36 @@ const Perfil = () => {
 
     return (
         <main>
-            <Link href="/perfil">
-                <a className="flex justify-center mt-10">
-                    <Image
-                        className="rounded-2xl"
-                        src={
-                            user?.picture ||
-                            "https://imgs.search.brave.com/95uqrX0sPNStAH-hNb9IC1Dq-eZQGRKKEUFV6PMDY_U/rs:fit:1200:1200:1/g:ce/aHR0cDovL3d3dy5k/cm9kZC5jb20vaW1h/Z2VzMTQvd2hpdGUx/LmpwZw"
-                        }
-                        alt={user?.name || "Não encontrado"}
-                        width="100"
-                        height="100"
-                    />
-                </a>
-            </Link>
+            <div className="flex justify-center mt-10 mb-96">
 
-            <h1 className="flex justify-center text-2xl mt-3 font-semibold">{user?.name}</h1>
+                <div className="flex justify-start bg-gray-200 w-4/5 h-64 rounded-lg">
+                    <div className="ml-12 mt-10">
+                        <Link href="/perfil">
+                            <a className="">
+                                <Image
+                                    className="rounded-full"
+                                    src={
+                                        user?.picture ||
+                                        "https://imgs.search.brave.com/95uqrX0sPNStAH-hNb9IC1Dq-eZQGRKKEUFV6PMDY_U/rs:fit:1200:1200:1/g:ce/aHR0cDovL3d3dy5k/cm9kZC5jb20vaW1h/Z2VzMTQvd2hpdGUx/LmpwZw"
+                                    }
+                                    alt={user?.name || "Não encontrado"}
+                                    width="170"
+                                    height="170"
+                                    />
+                            </a>
+                        </Link>
+                    </div>
+                    <div className="mt-10 ml-8">              
+                        <h1 
+                        className="flex justify-start text-[60px] font-semibold"
+                        >{user?.name}</h1>
+                        <h2 
+                        className="flex justify-start text-[20px] font-semibold text-justify w-[60rem] tracking-tighter"
+                        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h2>
+                    </div>  
+                </div>
+            </div>
         </main>
     );
 };
