5e5eb322e0f4987095e128e9085120d1020f5415
OGabrielPereira <ogabrielpereira@pm.me>
2022-08-21 22:28:48 -0400

    Author info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Root from "../graphs/components/Root";
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import axios from "axios";
 import { Dialog } from '@headlessui/react'
 
@@ -8,39 +8,52 @@ import {useUser} from "@auth0/nextjs-auth0";
 const Home = () => {
     const {user} = useUser();
     let [isOpen, setIsOpen] = useState(true);
+    let completeButtonRef = useRef(null)
+
     // Finding and createting user
     const findUser = async () => await axios.get(`/api/user/find/${user?.nickname}`);
     const createUser = async () => await axios.post('/api/user/create', user || {});
     
-    // Finding and createting author
-    const findAuthor = async () => await axios.get(`/api/author/find/${user?.nickname}`);
-    const createAuthor = async () => await axios.post('/api/author/create', user || {});
+    // Finding author
+    const findAuthor = async () => {
+        let find = await axios.get(`/api/author/find/${user?.nickname}`)
+        if (find.data === null) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+    // const createAuthor = async () => await axios.post('/api/author/create', user || {});
 
     function PopupHandler() {
-        if(user?.email?.includes("@ifc.edu.br")) {
-            findAuthor()
-                .then((res) => { 
-                    if(res.data === null) {
-                        createAuthor();
-                }
-            });
-        } else {
-            findUser()
-                .then((res) => { 
-                    if(res.data === null) {
-                        createUser();
-                }
-            });
-        }
+        findUser()
+            .then((res) => { 
+                if(res.data === null) {
+                    createUser();
+            }
+        });
         return setIsOpen(false);
     }
 
     return (
         <>  
             <main className="fixed h-screen w-full top-50 z-0">
-                <Dialog className="relative z-50" open={isOpen} onClose={() => PopupHandler()}>
-                </Dialog>
-                <Root />
+
+                {   user?.email?.includes("@ifc") ? (
+                    
+                    <h1>{user?.nickname}</h1>
+
+                ) : (
+                    <div>
+                        <Dialog className="relative z-40" open={isOpen} onClose={() => PopupHandler()}>
+                        </Dialog>
+                        <Root />
+                    </div>
+                )} 
+
+                
+
+                
             </main>
         </>
     );
