738959fc7fc8932c44fc8a6ba464df97df4161c2
Raphael-Soares <soaresphaell@gmail.com>
2022-08-23 20:24:59 -0300

    Pagina de autores estilizada

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,13 +7,12 @@ import {RiArrowDropDownLine} from "react-icons/ri";
 import {useUser} from "@auth0/nextjs-auth0";
 
 function toggleMenu() {
-    console.log('clicked');
+    console.log("clicked");
     const navToggle: any = document.getElementsByClassName("toggle");
     for (let i = 0; i < navToggle.length; i++) {
         navToggle.item(i).classList.toggle("hidden");
-  }
+    }
 }
-
 
 const NavBar = () => {
     const {user, error, isLoading} = useUser();
@@ -21,9 +20,7 @@ const NavBar = () => {
     return (
         <nav className="bg-black shadow-xl z-50 w-full">
             <div className="max-w-6xl mx-auto px-4">
-                <div
-                    className="flex justify-between"
-                >
+                <div className="flex justify-between">
                     <div className="flex">
                         <Link href="/">
                             <a className="flex items-center py-6 px-2">
@@ -31,16 +28,18 @@ const NavBar = () => {
                                 <span className="text-[#0952DB] font-semibold text-2xl tracking-tight pr-2">NODE</span>
                             </a>
                         </Link>
-                        
                     </div>
 
                     {/* RESPONSIVE */}
 
-                   <div className={"md:hidden w-full items-center md:w-auto mt-5 md:mt-0 border-t-2 md:border-none" + (navbarOpen ? " flex" : " hidden")}>
-                            <h3 className="block md:inline-block font-semibold text-white hover:text-blue-500 px-3 py-3 border-b-2 border-white md:border-none">Artigos</h3>
-                            <h3 className="block md:inline-block font-semibold text-white hover:text-blue-500 px-3 py-3 border-b-2 border-white md:border-none">Autores</h3>
-
-                        </div>
+                    <div className={"md:hidden w-full items-center md:w-auto mt-5 md:mt-0 border-t-2 md:border-none" + (navbarOpen ? " flex" : " hidden")}>
+                        <h3 className="block md:inline-block font-semibold text-white hover:text-blue-500 px-3 py-3 border-b-2 border-white md:border-none">
+                            Artigos
+                        </h3>
+                        <h3 className="block md:inline-block font-semibold text-white hover:text-blue-500 px-3 py-3 border-b-2 border-white md:border-none">
+                            Autores
+                        </h3>
+                    </div>
 
                     {/* <div className="flex md:hidden">
                         <button onClick={() => setNavbarOpen(!navbarOpen)} id="hamburger">
@@ -49,20 +48,17 @@ const NavBar = () => {
                         </button>
                     </div> */}
 
-                    
-
-
                     {/* RESPONSIVE END */}
-            
+
                     <div className="hidden md:flex items-center py-1 px-2">
                         {!user ? (
                             <Link href="/artigos">
-                                <a className="py-2 px-6 font-semibold rounded hover:bg-[#ffffff11] transition duration-300 mx-1">
+                                <a className="py-2 px-6 font-semibold rounded hover:bg-[#ffffff11] ease-out duration-300 mx-1">
                                     <span className="text-white">Artigos</span>
                                 </a>
                             </Link>
                         ) : (
-                            <div className="relative py-2 px-6 font-semibold rounded hover:bg-[#ffffff11] transition duration-300 mx-1">
+                            <div className="relative py-2 px-6 font-semibold rounded hover:bg-[#ffffff11] ease-out duration-300 mx-1">
                                 <DropdownItem user={user}>
                                     <span className="text-white flex items-center">
                                         <RiArrowDropDownLine className="text-white text-2xl" />
@@ -73,26 +69,26 @@ const NavBar = () => {
                         )}
 
                         <Link href="/autores">
-                            <a className="py-2 px-6 font-semibold rounded hover:bg-[#ffffff11] transition duration-300 mx-1">
+                            <a className="py-2 px-6 font-semibold rounded hover:bg-[#ffffff11] ease-out duration-300 mx-1">
                                 <span className="text-white">Autores</span>
                             </a>
                         </Link>
                         {!user && (
                             <Link href="/api/auth/login">
-                                <a className="py-2 px-6 font-semibold rounded hover:bg-[#ffffff11] transition duration-300 mx-1">
+                                <a className="py-2 px-6 font-semibold rounded hover:bg-[#ffffff11] ease-out duration-300 mx-1">
                                     <span className="text-white">Entrar</span>
                                 </a>
                             </Link>
                         )}
                         {user?.email?.includes("@ifc.edu.br") && (
                             <Link href="/">
-                                <a className="py-2 px-6 font-semibold rounded hover:bg-[#ffffff11] transition duration-300 mx-1">
+                                <a className="py-2 px-6 font-semibold rounded hover:bg-[#ffffff11] ease-out duration-300 mx-1">
                                     <span className="text-white">Adicionar Artigo</span>
                                 </a>
                             </Link>
                         )}
                         {user && (
-                            <div className="relative py-2 px-6 font-semibold rounded hover:bg-[#ffffff11] transition duration-300 mx-1">
+                            <div className="relative py-2 px-6 font-semibold rounded hover:bg-[#ffffff11] ease-out duration-300 mx-1">
                                 <DropdownUserItem user={user}>
                                     <span className="flex items-center">
                                         <RiArrowDropDownLine className="text-white text-2xl " />
@@ -108,10 +104,6 @@ const NavBar = () => {
                             </div>
                         )}
                     </div>
-
-
-
-
                 </div>
             </div>
         </nav>
--- pages/autores/index.tsx
+++ pages/autores/index.tsx
@@ -1,14 +1,13 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import axios from "axios";
+import {useEffect, useState} from "react";
+import Image from "next/image";
+import {BiChevronRight} from "react-icons/bi";
 
 const Autores = () => {
-
     const [authors, setAuthors] = useState<any[]>([]);
 
     useEffect(() => {
-        
-        axios.get('/api/author/find/all')
-            .then((res) => setAuthors(res.data));
+        axios.get("/api/author/find/all").then((res) => setAuthors(res.data));
         return () => {
             setAuthors([]);
         };
@@ -17,19 +16,40 @@ const Autores = () => {
 
     return (
         <main>
-            <h1 className="flex justify-center text-2xl">Autores</h1>
-            <h2>{
-                    // Teste
-                    authors.length > 0 && authors.map(
-                        (author) => (
-                            <li key={author.id} className="flex row justify-between m-2 bg-black drop-shadow-lg p-2 rounded">
-                                <h2 className='text-white'>{author.name}</h2>
-                            </li>
-                        )
-                    )
-
-                }
-            </h2>
+            <div className="flex row w-full justify-around p-4">
+                <input
+                    type="text"
+                    className="block p-4 mx-2 pl-10 w-4/5 text-sm text-white bg-black rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Pesquiar autores"
+                />
+                <select className="bg-black border w-1/5 border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5">
+                    <option selected>Filtar por</option>
+                </select>
+            </div>
+            <ul className="p-4">
+                {authors.length > 0 &&
+                    authors.map((author) => (
+                        <li key={author.id} className="flex row justify-between m-2 items-center bg-white drop-shadow-lg p-2 rounded">
+                            <header className="flex items-center text-bold p-1 ">
+                                <Image
+                                    className="rounded-full "
+                                    src={
+                                        author?.picture ||
+                                        "https://imgs.search.brave.com/95uqrX0sPNStAH-hNb9IC1Dq-eZQGRKKEUFV6PMDY_U/rs:fit:1200:1200:1/g:ce/aHR0cDovL3d3dy5k/cm9kZC5jb20vaW1h/Z2VzMTQvd2hpdGUx/LmpwZw"
+                                    }
+                                    alt={author?.name || "Não encontrado"}
+                                    width="75"
+                                    height="75"
+                                />
+                                <main className="flex flex-col mx-4">
+                                    <h2 className=" font-bold">{author.name}</h2>
+                                    <p className="text-gray-800 font-sm">{author.email}</p>
+                                </main>
+                            </header>
+                            <BiChevronRight className=" text-4xl ease-out duration-300 rounded-full hover:text-[#0952DB] hover:bg-[#00000011]" />
+                        </li>
+                    ))}
+            </ul>
         </main>
     );
 };
