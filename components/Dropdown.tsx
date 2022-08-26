b80fc29d698f80b0ad6d664d998fe11bf1451fee
Raphael-Soares <soaresphaell@gmail.com>
2022-08-26 19:58:28 -0300

    Dropdown z index fixed

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -27,28 +27,33 @@ function Dropdown({children, items}: any) {
         <>
             <span onClick={toggle} className="cursor-pointer ">
                 {children}
-                <div className={`${isOpen ? "" : "hidden"} absolute mt-2 mr-auto  bg-stone-900 rounded divide-y divide-gray-100 shadow`}>
-                    <ul className="py-1 text-sm text-gray-700 dark:text-gray-200 z-auto">
-                        {items.map(
-                            (item: any) =>
-                                item.show &&
-                                (item.to ? (
-                                    <li>
-                                        <Link href={item.to}>
-                                            <a className={`block py-2 px-4 hover:bg-stone-800 text-white ${item.destaque && "text-red-500"}`}>{item.label}</a>
-                                        </Link>
-                                    </li>
-                                ) : (
-                                    <li>
-                                        <a onClick={item.action} className={`block py-2 px-4 hover:bg-stone-800 text-white ${item.destaque && "text-red-500"}`}>
+            </span>
+            <div className={`${!isOpen && "hidden"} absolute mt-2 left-0 bg-stone-900 rounded divide-y divide-gray-100 shadow`}>
+                <ul className="p-1 text-sm text-gray-700 dark:text-gray-200 z-auto">
+                    {items.map(
+                        (item: any) =>
+                            item.show &&
+                            (item.to ? (
+                                <li>
+                                    <Link href={item.to}>
+                                        <a className={`block py-2 px-4 whitespace-nowrap hover:bg-stone-800 text-white ${item.destaque && "text-red-500"}`}>
                                             {item.label}
                                         </a>
-                                    </li>
-                                ))
-                        )}
-                    </ul>
-                </div>
-            </span>
+                                    </Link>
+                                </li>
+                            ) : (
+                                <li>
+                                    <a
+                                        onClick={item.action}
+                                        className={`block py-2 px-4 whitespace-nowrap hover:bg-stone-800 text-white ${item.destaque && "text-red-500"}`}
+                                    >
+                                        {item.label}
+                                    </a>
+                                </li>
+                            ))
+                    )}
+                </ul>
+            </div>
         </>
     );
 }
