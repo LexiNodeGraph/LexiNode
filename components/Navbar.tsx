eb5ee1928c4602b3cdc2eaeac8e834941ed64994
Raphael-Soares <soaresphaell@gmail.com>
2022-08-26 12:51:53 -0300

    mobile navbar ajustes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -151,37 +151,72 @@ const Navbar = () => {
                                 <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                                     {!user && (
                                         <Link href="/artigos">
-                                            <a className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">Artigos</a>
+                                            <a
+                                                className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                                                onClick={() => setIsOpen(!isOpen)}
+                                            >
+                                                Artigos
+                                            </a>
                                         </Link>
                                     )}
                                     {user && (
                                         <Link href="/artigos">
-                                            <a className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">Todos os Artigos</a>
+                                            <a
+                                                className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                                                onClick={() => setIsOpen(!isOpen)}
+                                            >
+                                                Todos os Artigos
+                                            </a>
                                         </Link>
                                     )}
                                     {user && (
                                         <Link href="/artigos/favoritos">
-                                            <a className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">Favoritos</a>
+                                            <a
+                                                className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                                                onClick={() => setIsOpen(!isOpen)}
+                                            >
+                                                Favoritos
+                                            </a>
                                         </Link>
                                     )}
 
                                     <Link href="/autores">
-                                        <a className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">Autores</a>
+                                        <a
+                                            className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                                            onClick={() => setIsOpen(!isOpen)}
+                                        >
+                                            Autores
+                                        </a>
                                     </Link>
                                     {!user && (
                                         <Link href="/api/auth/login">
-                                            <a className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">Entrar</a>
+                                            <a
+                                                className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                                                onClick={() => setIsOpen(!isOpen)}
+                                            >
+                                                Entrar
+                                            </a>
                                         </Link>
                                     )}
                                     {user && (
                                         <Link href="/perfil">
-                                            <a className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">Meu Perfil</a>
+                                            <a
+                                                className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                                                onClick={() => setIsOpen(!isOpen)}
+                                            >
+                                                Meu Perfil
+                                            </a>
                                         </Link>
                                     )}
 
                                     {user && (
                                         <Link href="/api/auth/logout">
-                                            <a className="hover:bg-gray-700 text-[#EF4444] block px-3 py-2 rounded-md text-base font-medium">Sair</a>
+                                            <a
+                                                className="hover:bg-gray-700 text-[#EF4444] block px-3 py-2 rounded-md text-base font-medium"
+                                                onClick={() => setIsOpen(!isOpen)}
+                                            >
+                                                Sair
+                                            </a>
                                         </Link>
                                     )}
                                 </div>
