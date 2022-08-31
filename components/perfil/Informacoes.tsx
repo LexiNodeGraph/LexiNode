9a50b6417ac297aa2e7721c5af52e03f159845be
Raphael-Soares <soaresphaell@gmai.com>
2022-08-31 08:48:08 -0300

    navegação no perfil

---
+++ components/perfil/Informacoes.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Informacoes() {
+    return (
+        <div>
+            <h2>Informações</h2>
+            <p>Vistos recentemente</p>
+            <p>Favoritos</p>
+        </div>
+    );
+}
+
+export default Informacoes;
--- components/perfil/UserContentCard.tsx
+++ components/perfil/UserContentCard.tsx
@@ -1,25 +1,28 @@
 import React from "react";
+import Link from "next/link";
 
-function UserContentCard() {
+function UserContentCard({children}: any) {
     return (
         <div className="w-full bg-white rounded-lg border shadow-md dark:bg-neutral-800 dark:border-neutral-700">
             <ul className="flex flex-wrap text-sm font-medium text-center text-neutral-500 bg-neutral-50 rounded-t-lg border-b border-neutral-200 dark:border-neutral-700 dark:text-neutral-400 dark:bg-neutral-800">
                 <li className="mr-2">
-                    <button className="inline-block p-4 text-blue-600 rounded-tl-lg hover:bg-neutral-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:text-blue-500 hover:text-blue-600 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500">
-                        Informações
-                    </button>
+                    <Link href="/perfil">
+                        <p className="inline-block p-4 text-blue-600 rounded-tl-lg hover:bg-neutral-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:text-blue-500 hover:text-blue-600 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500">
+                            Informações
+                        </p>
+                    </Link>
                 </li>
+
                 <li className="mr-2">
-                    <button className="inline-block p-4 hover:text-neutral-600 hover:bg-neutral-100 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:border-transparent text-neutral-500 dark:text-neutral-400 border-neutral-100 hover:border-neutral-300 dark:border-neutral-700">
-                        Favoritos
-                    </button>
-                </li>
-                <li className="mr-2">
-                    <button className="inline-block p-4 hover:text-neutral-600 hover:bg-neutral-100 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:border-transparent text-neutral-500 dark:text-neutral-400 border-neutral-100 hover:border-neutral-300 dark:border-neutral-700">
-                        Configurações
-                    </button>
+                    <Link href="/perfil/config">
+                        <p className="inline-block p-4 text-blue-600 rounded-tl-lg hover:bg-neutral-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:text-blue-500 hover:text-blue-600 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500">
+                            Configurações
+                        </p>
+                    </Link>
                 </li>
             </ul>
+
+            <div>{children}</div>
         </div>
     );
 }
