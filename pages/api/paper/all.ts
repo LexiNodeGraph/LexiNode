2eda5e66f06acb6c5f46591193edf32186fe8833
OGabrielPereira <ogabrielpereira@pm.me>
2022-08-31 09:04:02 -0300

    Teste 4

--- pages/api/paper/all.ts
+++ pages/api/paper/all.ts
@@ -1,9 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { findAllPapers } from "../db/search";
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse) {
-
+        
     res.status(200).json({
         message: "ok"
     });
