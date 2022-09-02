021604a30bb2b63ddcc8034c41e93a3effe70077
Raphael-Soares <soaresphaell@gmai.com>
2022-09-02 13:05:45 -0300

    dados do modal de edição de perfil atualizados

--- components/perfil/EditarPerfilModal.tsx
+++ components/perfil/EditarPerfilModal.tsx
@@ -8,7 +8,6 @@ function EditarPerfilModal({showModal, setShowModal}: any) {
                     <button
                         type="button"
                         className="absolute top-3 right-2.5 text-neutral-400 bg-transparent hover:bg-neutral-200 hover:text-neutral-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-neutral-800 dark:hover:text-white"
-                        data-modal-toggle="authentication-modal"
                     >
                         <button onClick={() => setShowModal(false)}>
                             <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -21,65 +20,39 @@ function EditarPerfilModal({showModal, setShowModal}: any) {
                         </button>
                     </button>
                     <div className="py-6 px-6 lg:px-8">
-                        <h3 className="mb-4 text-xl font-medium text-neutral-900 dark:text-white">Sign in to our platform</h3>
+                        <h3 className="mb-4 text-xl font-medium text-neutral-900 dark:text-white">Edite suas informações</h3>
                         <form className="space-y-6" action="#">
                             <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-neutral-900 dark:text-gray-300">
-                                    Your email
+                                <label htmlFor="biografia" className="block mb-2 text-sm font-medium text-neutral-900 dark:text-gray-300">
+                                    Biografia
                                 </label>
                                 <input
-                                    type="email"
-                                    name="email"
-                                    id="email"
+                                    type="text"
+                                    id="biografia"
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                                    placeholder="instituição"
+                                    placeholder="Biografia"
                                     required
                                 />
                             </div>
                             <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                                    Your password
+                                <label htmlFor="localização" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                    Localização
                                 </label>
                                 <input
-                                    type="password"
-                                    name="password"
-                                    id="password"
-                                    placeholder="••••••••"
+                                    id="localização"
+                                    type="text"
+                                    placeholder="Localização"
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                     required
                                 />
                             </div>
-                            <div className="flex justify-between">
-                                <div className="flex items-start">
-                                    <div className="flex items-center h-5">
-                                        <input
-                                            id="remember"
-                                            type="checkbox"
-                                            value=""
-                                            className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                                            required
-                                        />
-                                    </div>
-                                    <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                                        Remember me
-                                    </label>
-                                </div>
-                                <a href="#" className="text-sm text-blue-700 hover:underline dark:text-blue-500">
-                                    Lost Password?
-                                </a>
-                            </div>
+
                             <button
                                 type="submit"
                                 className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                             >
-                                Login to your account
+                                Salvar alterações
                             </button>
-                            <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                                Not registered?{" "}
-                                <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">
-                                    Create account
-                                </a>
-                            </div>
                         </form>
                     </div>
                 </div>
