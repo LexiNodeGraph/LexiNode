b68717e0aed73038744ac734bc3d179630d3c9e0
OGabrielPereira <ogabrielpereira@pm.me>
2022-09-09 14:25:29 -0300

    sextou

--- components/artigos/Form.tsx
+++ components/artigos/Form.tsx
@@ -21,31 +21,45 @@ const Form = () => {
       setLoop(allNames);
   }
 
-  const [formValue, setformValue] = useState({ // useState
+  const [paperForm, setPaperForm] = useState({ // useState
     title: '',
     journal_title: '',
-    field: '',
+    research_field: '',
     year: '',
     international: '',  
     web_link: '',
-    keyword: '',    
     abstract: '',
-    first_name0: '',
+    keyword: '', 
+    authors: [{}]   
   });
+
+  const [authorForm, setAuthorForm] = useState({
+    name: '',   
+    email: '',
+    institution: '',
+    author_role: '',
+    city: '',
+    country: '',
+    field: ''  
+  }); // useState
 
   const handleSubmit = async () => {
     
     let body = {
-      title: formValue.title,
-      journal_title: formValue.journal_title,
-      field: formValue.field,
-      year: formValue.year,
-      international: formValue.international,
-      web_link: formValue.web_link,
-      keyword: formValue.keyword,
-      abstract: formValue.abstract,
-      first_name0: formValue.first_name0,
+      title: paperForm.title,
+      journal_title: paperForm.journal_title,
+      field: paperForm.research_field,
+      year: paperForm.year,
+      international: paperForm.international,
+      web_link: paperForm.web_link,
+      keyword: paperForm.keyword,
+      abstract: paperForm.abstract,
     }
+
+    let authors = {
+      name: authorForm.name,
+
+    };
 
     try {
       // make axios post request
@@ -60,8 +74,8 @@ const Form = () => {
     }
   }
   const handleChange = (event:any) => {
-    setformValue({
-      ...formValue,
+    setPaperForm({
+      ...paperForm,
       [event.target.name]: event.target.value
     });
   }
@@ -74,51 +88,51 @@ const Form = () => {
           <div className="grid">
             <label htmlFor="name">Título</label>
             <input className="border border-1" type="text" name="title" 
-              value={formValue.title}
+              value={paperForm.title}
               onChange={() => handleChange}
             />
           </div>
           <div className="grid">
             <label htmlFor="name">Título do Journal</label>
             <input className="border border-1" type="text" name="journal_title"
-              value={formValue.journal_title}
+              value={paperForm.journal_title}
               onChange={() => handleChange} 
             />
           </div>
           <div className="grid">
             <label htmlFor="name">Área de pesquisa</label>
             <input className="border border-1" type="text" name="field" 
-            value={formValue.field}
+              value={paperForm.research_field}
             onChange={() => handleChange} />
           </div>
           <div className="grid">
             <label htmlFor="name">Ano de publicação</label>
             <input className="border border-1" type="text" name="year" 
-            value={formValue.year}
+              value={paperForm.year}
             onChange={() => handleChange} />
           </div>
           <div className="grid">
             <label htmlFor="name">Internacional(Sim/Não)</label>
             <input className="border border-1" type="text" name="international" 
-            value={formValue.international}
+              value={paperForm.international}
             onChange={() => handleChange} />
           </div>
           <div className="grid">
             <label htmlFor="name">Link Web</label>
             <input className="border border-1" type="text" name="web_link" 
-            value={formValue.web_link}
+              value={paperForm.web_link}
             onChange={() => handleChange} />
           </div>
           <div className="grid">
             <label htmlFor="name">Palavras-chave:</label>
             <input className="border border-1" type="text" name="keyword" 
-            value={formValue.keyword}
+              value={paperForm.keyword}
             onChange={() => handleChange} />
           </div>
           <div className="grid">
             <label htmlFor="name">Resumo</label>
             <input className="border border-1" type="text" name="abstract" 
-            value={formValue.abstract}
+              value={paperForm.abstract}
             onChange={() => handleChange} />
           </div>
           <div className="flex gap-2">
@@ -139,50 +153,40 @@ const Form = () => {
                   <div key={index}>
                     <hr className="border-2 mt-2 border-black"/>
                     <div className="grid">
-                      <label>Primeiro nome</label>
-                      <input className="border border-1" type="text" name={"first_name" + index} 
-                      value={formValue.first_name0}
+                      <label>Nome Completo</label>
+                      <input className="border border-1" type="text" name="first_name" 
+                        value={authorForm.name}
                       onChange={handleChange}/>
-                    
-                    </div>
-                    <div className="grid">
-                      <label>Nome do meio</label>
-                      <input className="border border-1" type="text" name={"middle_name" + item} />
-                    
-                    </div>
-                    <div className="grid">
-                      <label>Último nome</label>  
-                      <input className="border border-1" type="text" name={"last_name" + item} />
-                    
+
                     </div>
                     <div className="grid">
                       <label>Email</label>  
-                      <input className="border border-1" type="text" name={"email" + item} />
+                      <input className="border border-1" type="text" name="email" />
                     
                     </div>
                     <div className="grid">
                       <label>Instituição</label>  
-                      <input className="border border-1" type="text" name={"institution" + item} />
+                      <input className="border border-1" type="text" name="institution" />
                     
                     </div>
                     <div className="grid">
                       <label>Autor principal/colaborador</label>  
-                      <input className="border border-1" type="text" name={"author_role" + item} />
+                      <input className="border border-1" type="text" name="author_role" />
                     
                     </div>
                     <div className="grid">
                       <label>Cidade</label>  
-                      <input className="border border-1" type="text" name={"city" + item} />
+                      <input className="border border-1" type="text" name="city" />
                     
                     </div>
                     <div className="grid">
                       <label>País</label>  
-                      <input className="border border-1" type="text" name={"country" + item} />
+                      <input className="border border-1" type="text" name="country" />
                     
                     </div>
                     <div className="grid">
                       <label>Área de atuação</label>  
-                      <input className="border border-1" type="text" name={"field" + item} />
+                      <input className="border border-1" type="text" name="field" />
                     
                     </div>
                   </div>
