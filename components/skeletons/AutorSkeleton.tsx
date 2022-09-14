9fa40a4efdf77df6f0cdf6956dd5ec5218653249
Raphael-Soares <soaresphaell@gmai.com>
2022-09-14 08:56:19 -0300

    Autor Skeletons

--- components/skeletons/AutorSkeleton.tsx
+++ components/skeletons/AutorSkeleton.tsx
@@ -45,6 +45,48 @@ function AutorSkeleton() {
                     <div className="w-44 bg-gray-300 h-4 rounded  dark:bg-neutral-700 dark:border-neutral-700"></div>
                 </div>
             </li>
+            <li className="flex row justify-between  bg-white drop-shadow-lg p-2 animate-pulse rounded  dark:bg-neutral-900 dark:border-neutral-700">
+                <div className="bg-white dark:bg-neutral-900 "></div>
+                <div className="flex flex-col w-full space-y-6 ">
+                    <div className="w-1/2 bg-gray-300 h-4 rounded-md  dark:bg-neutral-700 dark:border-neutral-700 "></div>
+                    <div className="w-44 bg-gray-300 h-4 rounded  dark:bg-neutral-700 dark:border-neutral-700"></div>
+                </div>
+            </li>
+            <li className="flex row justify-between  bg-white drop-shadow-lg p-2 animate-pulse rounded  dark:bg-neutral-900 dark:border-neutral-700">
+                <div className="bg-white dark:bg-neutral-900 "></div>
+                <div className="flex flex-col w-full space-y-6 ">
+                    <div className="w-1/2 bg-gray-300 h-4 rounded-md  dark:bg-neutral-700 dark:border-neutral-700 "></div>
+                    <div className="w-44 bg-gray-300 h-4 rounded  dark:bg-neutral-700 dark:border-neutral-700"></div>
+                </div>
+            </li>
+            <li className="flex row justify-between  bg-white drop-shadow-lg p-2 animate-pulse rounded  dark:bg-neutral-900 dark:border-neutral-700">
+                <div className="bg-white dark:bg-neutral-900 "></div>
+                <div className="flex flex-col w-full space-y-6 ">
+                    <div className="w-1/2 bg-gray-300 h-4 rounded-md  dark:bg-neutral-700 dark:border-neutral-700 "></div>
+                    <div className="w-44 bg-gray-300 h-4 rounded  dark:bg-neutral-700 dark:border-neutral-700"></div>
+                </div>
+            </li>
+            <li className="flex row justify-between  bg-white drop-shadow-lg p-2 animate-pulse rounded  dark:bg-neutral-900 dark:border-neutral-700">
+                <div className="bg-white dark:bg-neutral-900 "></div>
+                <div className="flex flex-col w-full space-y-6 ">
+                    <div className="w-1/2 bg-gray-300 h-4 rounded-md  dark:bg-neutral-700 dark:border-neutral-700 "></div>
+                    <div className="w-44 bg-gray-300 h-4 rounded  dark:bg-neutral-700 dark:border-neutral-700"></div>
+                </div>
+            </li>
+            <li className="flex row justify-between  bg-white drop-shadow-lg p-2 animate-pulse rounded  dark:bg-neutral-900 dark:border-neutral-700">
+                <div className="bg-white dark:bg-neutral-900 "></div>
+                <div className="flex flex-col w-full space-y-6 ">
+                    <div className="w-1/2 bg-gray-300 h-4 rounded-md  dark:bg-neutral-700 dark:border-neutral-700 "></div>
+                    <div className="w-44 bg-gray-300 h-4 rounded  dark:bg-neutral-700 dark:border-neutral-700"></div>
+                </div>
+            </li>
+            <li className="flex row justify-between  bg-white drop-shadow-lg p-2 animate-pulse rounded  dark:bg-neutral-900 dark:border-neutral-700">
+                <div className="bg-white dark:bg-neutral-900 "></div>
+                <div className="flex flex-col w-full space-y-6 ">
+                    <div className="w-1/2 bg-gray-300 h-4 rounded-md  dark:bg-neutral-700 dark:border-neutral-700 "></div>
+                    <div className="w-44 bg-gray-300 h-4 rounded  dark:bg-neutral-700 dark:border-neutral-700"></div>
+                </div>
+            </li>
         </>
     );
 }
