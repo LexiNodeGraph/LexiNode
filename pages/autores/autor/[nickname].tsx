b9ca8c7a9851c09536ee6276ef8db1817c04d0f2
Raphael-Soares <soaresphaell@gmail.com>
2022-09-22 22:13:22 -0300

    Página de autor individual && buscando artigos do autor

--- pages/autores/autor/[nickname].tsx
+++ pages/autores/autor/[nickname].tsx
@@ -1,20 +1,48 @@
-import React from "react";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import {useState, useEffect} from "react";
 import axios from "axios";
+import {useRouter} from "next/router";
+
+import Image from "next/image";
+
+import ArtigoSkeleton from "../../../components/skeletons/ArtigoSkeleton";
+import ArtigoItem from "../../../components/artigos/ArtigoItem";
 
 function Autor() {
-
     const router = useRouter();
-    const { nickname } = router.query;
-    const [author, setAuthor] = React.useState<any[]>([]);
-    
-    const findAuthor = async () => await axios.get(`/api/paper/find/authors/${nickname?.toString()}`);
+    const {nickname} = router.query;
+    const [artigos, setArtigos] = useState<any[]>([]);
+    const [autor, setAutor] = useState<any>([]);
+    const [authorPapers, setAuthorPapers] = useState<any[]>([]);
+
+    useEffect(() => {
+        axios.get("/api/paper/find/all").then((data) => setArtigos(data.data));
+
+        return () => {
+            setArtigos([]);
+        };
+    }, []);
+
+    useEffect(() => {
+        getAuthorPapers();
+    }, [artigos]);
+
+    function getAuthorPapers() {
+        artigos.forEach((artigo: any) => {
+            artigo.authors.forEach((author: any) => {
+                if (author.email == nickname) {
+                    setAuthorPapers((authorPapers) => [...authorPapers, artigo]);
+                }
+            });
+        });
+    }
+    console.log(authorPapers);
+    console.log(autor);
 
     return (
-        <>
-            <h1>Artigos do autor</h1>
-        </>
+        <div className="dark:bg-neutral-900 dark">
+            <h1>{nickname}</h1>
+            <ul>{authorPapers.length > 0 ? authorPapers.map((artigo) => <ArtigoItem artigo={artigo} key={artigo.id} />) : <ArtigoSkeleton />}</ul>
+        </div>
     );
 }
 
