a794d3b25a4cce9aa1806a51dbc1ac23999adb10
Raphael-Soares <soaresphaell@gmail.com>
2022-10-01 13:39:39 -0300

    Keyword pae styled

--- pages/artigos/keyword/[keyword].tsx
+++ pages/artigos/keyword/[keyword].tsx
@@ -1,27 +1,55 @@
-import React from "react";
 import {useEffect, useState} from "react";
 import {useRouter} from "next/router";
+import axios from "axios";
+
 import ArtigoItem from "../../../components/artigos/ArtigoItem";
 import ArtigoSkeleton from "../../../components/skeletons/ArtigoSkeleton";
-import axios from "axios";
+import FilterForm from "../../../components/FilterForm";
 
 function Autor() {
     const router = useRouter();
-    const { keyword } = router.query;
-    const [key, setKey] = useState<any[]>([]);
-    useEffect(() => { 
-        axios.get(`/api/paper/find/keywords/${keyword}`).then((data: any) => setKey(data.data));
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [keyword, key]);
+    const {keyword} = router.query;
+    const [artigos, setArtigos] = useState<any[]>([]);
+
+    const [ordenar, setOrdenar] = useState();
+    const [input, setInput] = useState("");
+
+    const filtredArtigos = artigos.filter((artigo) => artigo.title.toLowerCase().includes(input));
+    useEffect(() => {
+        axios.get(`/api/paper/find/keywords/${keyword}`).then((data: any) => setArtigos(data.data));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [keyword]);
 
     return (
         <div>
-            <h1>
-                Artigos com a palavra-chave --{">"} {keyword}
-            </h1>
-            {key.map((artigo: any) => (
-                <ArtigoItem artigo={artigo} key={artigo.id} />
-            ))}
+            <div className=" p-4 dark:bg-neutral-900 dark:border-neutral-700">
+                <h1 className="text-center text-4xl font-bold text-neutral-900 dark:text-neutral-200">{keyword}</h1>
+            </div>
+            <div
+                className="
+                gap-2  p-2 pr-4 w-full flex flex-col-reverse
+                sm:flex-row
+                dark:bg-neutral-900 dark:border-neutral-700
+              "
+            >
+                <div className=" w-full h-fit bg-white rounded  shadow  dark:bg-neutral-900  dark:border-neutral-700">
+                    <FilterForm input={input} ordenar={ordenar} setOrdenar={setOrdenar} setInput={setInput} />
+                    {artigos.length > 0 ? (
+                        ordenar ? (
+                            filtredArtigos
+                                .sort((a, b) => (a.title > b.title ? 1 : b.title > a.title ? -1 : 0))
+                                .map((artigo) => <ArtigoItem artigo={artigo} key={artigo.id} />)
+                        ) : (
+                            filtredArtigos
+                                .sort((a, b) => (a.title > b.title ? 1 : b.title > a.title ? -1 : 0))
+                                .reverse()
+                                .map((artigo) => <ArtigoItem artigo={artigo} key={artigo.id} />)
+                        )
+                    ) : (
+                        <ArtigoSkeleton />
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
