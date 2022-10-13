ebe173d27521ee1e597959280e130f52a56f8a26
OGabrielPereira <ogabrielpereira@pm.me>
2022-10-13 10:24:40 -0300

    configuration new route

---
+++ pages/api/paper/find/authors/paper/[author].ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { findAllPapers } from "../../../../db/search";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse) {
+
+    const query = req.query;
+    const { author } = query;
+
+    res.status(200).json(author);
+}
