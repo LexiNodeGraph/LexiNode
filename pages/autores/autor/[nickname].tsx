2b2ecf08d0565eb25bf7d689cefac91e01b2a96c
Raphael-Soares <soaresphaell@gmail.com>
2022-10-14 15:54:43 -0300

    rota de busca de artigos do autor alterada

--- pages/autores/autor/[nickname].tsx
+++ pages/autores/autor/[nickname].tsx
@@ -14,7 +14,7 @@ function Autor() {
     const [artigos, setArtigos] = useState<any[]>([]);
 
     useEffect(() => {
-        axios.get("/api/paper/find/all").then((data) => getAuthorPapers(data.data));
+        axios.get(`/api/paper/find/authors/paper/${nickname}`).then((data) => setArtigos(data.data));
         axios.get(`/api/paper/find/authors/${nickname}`).then((data) => setAutor(data.data[0]));
         // eslint-disable-next-line react-hooks/exhaustive-deps
 
@@ -23,18 +23,6 @@ function Autor() {
         };
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-
-    // Rota pra achar os dados dos autores /api/paper/find/authors/{nickname}
-
-    function getAuthorPapers(papers: any[]) {
-        papers.forEach((artigo: any) => {
-            artigo.authors.forEach((author: any) => {
-                if (author.email == nickname) {
-                    setArtigos((artigos) => [...artigos, artigo]);
-                }
-            });
-        });
-    }
 
     return (
         <div className="inline-flex w-full gap-2 p-2 min-h-screen bg-neutral-900">
