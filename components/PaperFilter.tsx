d6faafca3201ec84c8d98b6dae11a329a50d1baf
Raphael-Soares <soaresphaell@gmail.com>
2023-03-21 10:43:39 -0300

    melhoras de performance no PaperFilter

--- components/PaperFilter.tsx
+++ components/PaperFilter.tsx
@@ -1,33 +1,40 @@
-import {useState} from "react";
-import {useUser} from "@auth0/nextjs-auth0";
+import { useState, useMemo } from "react";
+import { useUser } from "@auth0/nextjs-auth0";
 
 import ArtigoSkeleton from "./skeletons/ArtigoSkeleton";
 import ArtigoItem from "./artigos/ArtigoItem";
 import FilterForm from "./FilterForm";
 
-function PaperFilter({artigos, loading}: any) {
-    const {user} = useUser();
+function PaperFilter({ artigos, loading }: any) {
+    const { user } = useUser();
 
-    const [ordenar, setOrdenar] = useState();
+    const [ordenar, setOrdenar] = useState(false);
     const [input, setInput] = useState("");
 
-    const filtredArtigos = artigos.filter((artigo: any) => artigo.title.toLowerCase().includes(input));
+    const filteredArtigos = useMemo(() => {
+        return artigos.filter((artigo: any) => artigo.title.toLowerCase().includes(input));
+    }, [artigos, input]);
+
+    const sortedArtigos = useMemo(() => {
+        return filteredArtigos.sort((a: any, b: any) => {
+            return ordenar ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title);
+        });
+    }, [filteredArtigos, ordenar]);
+
     return (
         <div className=" w-full h-fit bg-white rounded  shadow  dark:bg-neutral-900  dark:border-neutral-700">
-            <FilterForm input={input} ordenar={ordenar} setOrdenar={setOrdenar} setInput={setInput} />
-            {artigos.length > 0 ? (
-                ordenar ? (
-                    filtredArtigos
-                        .sort((a: any, b: any) => (a.title > b.title ? 1 : b.title > a.title ? -1 : 0))
-                        .map((artigo: any) => <ArtigoItem artigo={artigo} user={user} key={artigo.id} />)
-                ) : (
-                    filtredArtigos
-                        .sort((a: any, b: any) => (a.title > b.title ? 1 : b.title > a.title ? -1 : 0))
-                        .reverse()
-                        .map((artigo: any) => <ArtigoItem artigo={artigo} user={user} key={artigo.id} />)
-                )
-            ) : (
+            <FilterForm
+                input={input}
+                ordenar={ordenar}
+                setOrdenar={setOrdenar}
+                setInput={setInput}
+            />
+            {loading ? (
                 <ArtigoSkeleton />
+            ) : (
+                sortedArtigos.map((artigo: any) => (
+                    <ArtigoItem artigo={artigo} user={user} key={artigo.id} />
+                ))
             )}
         </div>
     );
