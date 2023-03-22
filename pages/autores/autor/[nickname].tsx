8a800fae450d2cfd5d016c4cd38e1857cb3f5ee4
Raphael-Soares <soaresphaell@gmail.com>
2023-03-22 15:05:29 -0300

    performance fixes

--- pages/autores/autor/[nickname].tsx
+++ pages/autores/autor/[nickname].tsx
@@ -1,5 +1,5 @@
-import {useRouter} from "next/router";
-import {useState, useEffect} from "react";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 import PaperFilter from "../../../components/PaperFilter";
@@ -8,21 +8,22 @@ import UserCard from "../../../components/perfil/UserCard";
 
 function Autor() {
     const router = useRouter();
-    const {nickname} = router.query;
+    const { nickname } = router.query;
 
     const [autor, setAutor] = useState<any>({});
     const [artigos, setArtigos] = useState<any[]>([]);
 
     useEffect(() => {
-        axios.get(`/api/paper/find/authors/paper/${nickname}`).then((data) => setArtigos(data.data));
-        axios.get(`/api/paper/find/authors/${nickname}`).then((data) => setAutor(data.data[0]));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-
-        return () => {
-            setArtigos([]);
-        };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+        async function fetchData() {
+            const [artigosData, autorData] = await Promise.all([
+                axios.get(`/api/paper/find/authors/paper/${nickname}`),
+                axios.get(`/api/paper/find/authors/${nickname}`),
+            ]);
+            setArtigos(artigosData.data);
+            setAutor(autorData.data[0]);
+        }
+        fetchData();
+    }, [nickname]);
 
     return (
         <div className="inline-flex w-full gap-2 p-2 min-h-screen dark:bg-neutral-900 bg-neutral-50">
