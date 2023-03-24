7fd61db3e566b1343cd0ac42b98411fc3977c845
Raphael-Soares <soaresphaell@gmail.com>
2023-03-24 09:53:16 -0300

    spinner fixed

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -3,8 +3,8 @@ import { FaSpinner } from "react-icons/fa";
 
 const Spinner = () => {
     return (
-        <div className="spinner h-wh">
-            <FaSpinner className="spinner-icon" />
+        <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center bg-gray-200">
+            <FaSpinner className="animate-spin text-4xl text-gray-500" />
         </div>
     );
 };
