b6e02512535d2e58fddb7bb4b698bc5d78ea0e88
Raphael-Soares <soaresphaell@gmail.com>
2023-03-24 09:25:56 -0300

    performance na home

--- graphs/components/Root.tsx
+++ graphs/components/Root.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useMemo, useState, useCallback } from "react";
 import { SigmaContainer, ZoomControl, FullScreenControl } from "react-sigma-v2";
 import getNodeProgramImage from "sigma/rendering/webgl/programs/node.image";
 
@@ -17,71 +17,30 @@ const Root: FC = () => {
     const [showContents, setShowContents] = useState(false);
     const [dataReady, setDataReady] = useState(false);
     const [data, setData] = useState<any[] | null>(null);
-
     const [hoveredNode, setHoveredNode] = useState<string | null>(null);
 
-    let url: string;
-
-    if (typeof window !== "undefined") {
-        window.location.protocol == 'http:' ? url = 'http://localhost:3000' : url = 'https://lexinode.vercel.app';
-    }
+    const url = useMemo(() => {
+        if (typeof window !== "undefined") {
+            return window.location.protocol === "http:"
+                ? "http://localhost:3000"
+                : "https://lexinode.vercel.app";
+        }
+    }, []);
 
     // Load data on mount:
     useEffect(() => {
-        //change fetch to axios
-        axios.get(`${url}/api/paper/find/keywords/all`)
-            .then((data: any) => {
-                setData(data.data);
-                requestAnimationFrame(() => setDataReady(true));
-            });
-    }, []);
+        axios.get(`${url}/api/paper/find/keywords/all`).then((response) => {
+            setData(response.data);
+            requestAnimationFrame(() => setDataReady(true));
+        });
+    }, [url]);
 
-    if (!data) return (
-        <div id="app-root" className={"z-0" + (showContents ? "show-contents" : "")}>
-            <SigmaContainer
-                initialSettings={{
-                    nodeProgramClasses: { image: getNodeProgramImage() },
-                    labelRenderer: drawLabel,
-                    defaultNodeType: "image",
-                    defaultEdgeType: "line",
-                    labelDensity: 0.1,
-                    labelGridCellSize: 60,
-                    labelRenderedSizeThreshold: 15,
-                    labelFont: "Lato, sans-serif",
-                    zIndex: true,
-                    allowInvalidContainer: true
-
-                }}
-                className="react-sigma"
-            >
-
-
-                <>
-                    <div className="controls">
-                        <div className="ico">
-                            <button type="button" className="show-contents" onClick={() => setShowContents(true)} title="Show caption and description">
-                                <BiBookContent />
-                            </button>
-                        </div>
-                        <FullScreenControl className="ico" customEnterFullScreen={<BsArrowsFullscreen />} customExitFullScreen={<BsFullscreenExit />} />
-                        <ZoomControl className="ico" customZoomIn={<BsZoomIn />} customZoomOut={<BsZoomOut />} customZoomCenter={<BiRadioCircleMarked />} />
-                    </div>
-                    <div className="contents">
-                        <div className="ico">
-                            <button type="button" className="ico hide-contents" onClick={() => setShowContents(false)} title="Show caption and description">
-                                <GrClose />
-                            </button>
-                        </div>
-                        <div className="panels">
-                        </div>
-                    </div>
-                </>
-            </SigmaContainer>
-        </div>
-    );
+    if (!data) {
+        return null; // or a loading spinner, etc.
+    }
 
     return (
-        <div id="app-root" className={"z-0" + (showContents ? "show-contents" : "")}>
+        <div id="app-root" className={`z-0 ${showContents ? "show-contents" : ""}`}>
             <SigmaContainer
                 initialSettings={{
                     nodeProgramClasses: { image: getNodeProgramImage() },
@@ -93,8 +52,7 @@ const Root: FC = () => {
                     labelRenderedSizeThreshold: 15,
                     labelFont: "Lato, sans-serif",
                     zIndex: true,
-                    allowInvalidContainer: true
-
+                    allowInvalidContainer: true,
                 }}
                 className="react-sigma"
             >
@@ -102,27 +60,44 @@ const Root: FC = () => {
                 <GraphSettingsController hoveredNode={hoveredNode} />
                 <GraphDataController dataset={data} />
 
-        
-                    <>
-                        <div className="controls">
-                            <div className="ico">
-                                <button type="button" className="show-contents" onClick={() => setShowContents(true)} title="Show caption and description">
-                                    <BiBookContent />
-                                </button>
-                            </div>
-                            <FullScreenControl className="ico" customEnterFullScreen={<BsArrowsFullscreen />} customExitFullScreen={<BsFullscreenExit />} />
-                            <ZoomControl className="ico" customZoomIn={<BsZoomIn />} customZoomOut={<BsZoomOut />} customZoomCenter={<BiRadioCircleMarked />} />
+                <div className="controls">
+                    <div className="ico">
+                        <button
+                            type="button"
+                            className="show-contents"
+                            onClick={() => setShowContents(true)}
+                            title="Show caption and description"
+                        >
+                            <BiBookContent />
+                        </button>
+                    </div>
+                    <FullScreenControl
+                        className="ico"
+                        customEnterFullScreen={<BsArrowsFullscreen />}
+                        customExitFullScreen={<BsFullscreenExit />}
+                    />
+                    <ZoomControl
+                        className="ico"
+                        customZoomIn={<BsZoomIn />}
+                        customZoomOut={<BsZoomOut />}
+                        customZoomCenter={<BiRadioCircleMarked />}
+                    />
+                </div>
+                {showContents && (
+                    <div className="contents">
+                        <div className="ico">
+                            <button
+                                type="button"
+                                className="ico hide-contents"
+                                onClick={() => setShowContents(false)}
+                                title="Hide caption and description"
+                            >
+                                <GrClose />
+                            </button>
                         </div>
-                        <div className="contents">
-                            <div className="ico">
-                                <button type="button" className="ico hide-contents" onClick={() => setShowContents(false)} title="Show caption and description">
-                                    <GrClose />
-                                </button>
-                            </div>
-                            <div className="panels">
-                            </div>
-                        </div>
-                    </>
+                        <div className="panels">{/* contents */}</div>
+                    </div>
+                )}
             </SigmaContainer>
         </div>
     );
