c3b04f36d9c6995d02404814894b1d1e95c4287a
Raphael-Soares <soaresphaell@gmail.com>
2023-03-28 08:29:53 -0300

    skeleton na pargina de artigos arrumado

--- components/skeletons/ArtigoSkeleton.jsx
+++ components/skeletons/ArtigoSkeleton.jsx
@@ -1,50 +1,22 @@
 import React from "react";
 
 function ArtigoSkeleton() {
+    function Artigo() {
+        return (
+            <li className="flex row justify-between m-2 h-36 bg-white drop-shadow-lg p-2 animate-pulse rounded   dark:text-white  dark:bg-neutral-900  dark:border-neutral-700">
+                <div className="flex flex-col w-full space-y-6 ">
+                    <div className="w-1/2 bg-gray-300 h-6 rounded-md dark:bg-neutral-700"></div>
+                    <div className="w-44 bg-gray-300 h-4 rounded dark:bg-neutral-700"></div>
+                    <div className="w-1/4 h-6 my-2 p-2 rounded  bg-gray-300 dark:bg-neutral-700"></div>
+                </div>
+            </li>
+        );
+    }
     return (
         <>
-            <li className="flex row justify-between m-2 h-36 bg-white drop-shadow-lg p-2 animate-pulse rounded   dark:text-white  dark:bg-neutral-900  dark:border-neutral-700">
-                <div className="flex flex-col w-full space-y-6 ">
-                    <div className="w-1/2 bg-gray-300 h-6 rounded-md dark:bg-neutral-700"></div>
-                    <div className="w-44 bg-gray-300 h-4 rounded dark:bg-neutral-700"></div>
-                    <div className="w-1/4 h-6 my-2 p-2 rounded  bg-gray-300 dark:bg-neutral-700"></div>
-                </div>
-            </li>
-            <li className="flex row justify-between m-2 h-36 bg-white drop-shadow-lg p-2 animate-pulse rounded   dark:text-white  dark:bg-neutral-900  dark:border-neutral-700">
-                <div className="flex flex-col w-full space-y-6 ">
-                    <div className="w-1/2 bg-gray-300 h-6 rounded-md dark:bg-neutral-700"></div>
-                    <div className="w-44 bg-gray-300 h-4 rounded dark:bg-neutral-700"></div>
-                    <div className="w-1/4 h-6 my-2 p-2 rounded  bg-gray-300 dark:bg-neutral-700"></div>
-                </div>
-            </li>
-            <li className="flex row justify-between m-2 h-36 bg-white drop-shadow-lg p-2 animate-pulse rounded   dark:text-white  dark:bg-neutral-900  dark:border-neutral-700">
-                <div className="flex flex-col w-full space-y-6 ">
-                    <div className="w-1/2 bg-gray-300 h-6 rounded-md dark:bg-neutral-700"></div>
-                    <div className="w-44 bg-gray-300 h-4 rounded dark:bg-neutral-700"></div>
-                    <div className="w-1/4 h-6 my-2 p-2 rounded  bg-gray-300 dark:bg-neutral-700"></div>
-                </div>
-            </li>
-            <li className="flex row justify-between m-2 h-36 bg-white drop-shadow-lg p-2 animate-pulse rounded   dark:text-white  dark:bg-neutral-900  dark:border-neutral-700">
-                <div className="flex flex-col w-full space-y-6 ">
-                    <div className="w-1/2 bg-gray-300 h-6 rounded-md dark:bg-neutral-700"></div>
-                    <div className="w-44 bg-gray-300 h-4 rounded dark:bg-neutral-700"></div>
-                    <div className="w-1/4 h-6 my-2 p-2 rounded  bg-gray-300 dark:bg-neutral-700"></div>
-                </div>
-            </li>
-            <li className="flex row justify-between m-2 h-36 bg-white drop-shadow-lg p-2 animate-pulse rounded   dark:text-white  dark:bg-neutral-900  dark:border-neutral-700">
-                <div className="flex flex-col w-full space-y-6 ">
-                    <div className="w-1/2 bg-gray-300 h-6 rounded-md dark:bg-neutral-700"></div>
-                    <div className="w-44 bg-gray-300 h-4 rounded dark:bg-neutral-700"></div>
-                    <div className="w-1/4 h-6 my-2 p-2 rounded  bg-gray-300 dark:bg-neutral-700"></div>
-                </div>
-            </li>
-            <li className="flex row justify-between m-2 h-36 bg-white drop-shadow-lg p-2 animate-pulse rounded   dark:text-white  dark:bg-neutral-900  dark:border-neutral-700">
-                <div className="flex flex-col w-full space-y-6 ">
-                    <div className="w-1/2 bg-gray-300 h-6 rounded-md dark:bg-neutral-700"></div>
-                    <div className="w-44 bg-gray-300 h-4 rounded dark:bg-neutral-700"></div>
-                    <div className="w-1/4 h-6 my-2 p-2 rounded  bg-gray-300 dark:bg-neutral-700"></div>
-                </div>
-            </li>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+                <Artigo key={item} />
+            ))}
         </>
     );
 }
--- pages/artigos/index.tsx
+++ pages/artigos/index.tsx
@@ -3,13 +3,17 @@ import axios from "axios";
 
 import PaperFilter from "../../components/PaperFilter";
 
+import ArtigoSkeleton from "../../components/skeletons/ArtigoSkeleton";
+
 const Artigos = () => {
     const [artigos, setArtigos] = useState<any[]>([]);
+    const [isLoading, setIsLoading] = useState<any>(true);
 
     const fetchArtigos = useCallback(async () => {
         try {
             const response = await axios.get("/api/paper/find/all");
             setArtigos(response.data);
+            setIsLoading(false);
         } catch (error) {
             console.error(error);
         }
@@ -24,7 +28,7 @@ const Artigos = () => {
 
     return (
         <div className="gap-2 p-2 pr-4 w-full min-h-screen flex flex-col-reverse sm:flex-row dark:bg-neutral-900 dark:border-neutral-700">
-            <PaperFilter artigos={artigos} />
+            {isLoading ? <PaperFilter artigos={artigos} /> : <ArtigoSkeleton />}
         </div>
     );
 };
